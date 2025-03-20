0a2eda1288ce78c6ca641c60a69481f0d6aa5ecd
SUPUN-WINDOWS\User <supun@ensiz.com>
2025-03-20 14:59:28 +0530

    change env file

--- environment.ts
+++ environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
     production: true,
-    baseURL: 'https://f098-2402-4000-2390-57d-57be-de2f-59b0-4972.ngrok-free.app',
+    baseURL: 'https://fc6f-123-231-9-44.ngrok-free.app',
     key: 'FGFygbvxywvdmgiszzmdjgdfujkquwsDEQDABjkdnNC'
   };
   
