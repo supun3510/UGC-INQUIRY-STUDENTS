f22ba83bf0039b151cf89054960b0d0c567fc984
SUPUN-WINDOWS\User <supun@ensiz.com>
2025-08-09 22:28:11 +0530

    baseurl updated

--- environment.ts
+++ environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
     production: true,
-    baseURL: 'https://ac67-2402-4000-23a0-2d7f-4266-b858-f22d-b353.ngrok-free.app',
+    baseURL: 'https://b1a3e6188207.ngrok-free.app',
     key: 'FGFygbvxywvdmgiszzmdjgdfujkquwsDEQDABjkdnNC'
   };
   
